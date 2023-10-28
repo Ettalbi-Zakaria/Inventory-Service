fc052f6f0af6eb650317a213c5caf804b3b4388f
Ettalbi-Zakaria <z.ettalbi@etu.enset-media.ac.ma>
2023-10-28 19:49:35 +0100

    inventory-service done

--- src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,15 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository){
+        return args -> {
+          productRepository.save(new Product(null, "Laptop", 2000, 100));
+          productRepository.save(new Product(null, "Telephone", 1500, 150));
+          productRepository.save(new Product(null, "Clavier", 1000, 200));
+          productRepository.findAll().forEach(product -> {
+              System.out.println(product.getName());
+          });
+        };
+    }
 }
